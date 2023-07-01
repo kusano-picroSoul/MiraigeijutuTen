8a40c9a50c27ee7b6628e58eb1680f9b3a6b8f2f
Uhiroya <72613038+Uhiroya@users.noreply.github.com>
2023-07-01 18:24:51 +0900

    プレイヤーの基本の型の追加

---
+++ MiraigeijutuTenGame/Assets/UedaFolder/Scripts/PlayerClass.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Condition
+{
+    Normal,
+    Hungry,
+    Angry,
+    Happy,
+    Stupid,
+    Tired,
+}
---
+++ MiraigeijutuTenGame/Assets/UedaFolder/Scripts/PlayerStatus.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour
+{
+    public int Hungry = 50;
+    public int Happy = 50;
+    public int Smart = 50;
+    public int Health = 50;
+    public Condition PlayerCondition = Condition.Normal;
+
+    private PlayerAnimation _playerAnimation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerAnimation = GetComponent<PlayerAnimation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerConditionUpdate();
+        PlayerConditionMoving();
+    }
+
+    ////プレイヤーコンディションの管理
+    public void PlayerConditionUpdate()
+    {
+        if (Health <= 0 )
+        {
+            Hungry = 0;
+            PlayerCondition = Condition.Tired;
+        }
+        else if (Hungry <= 0)
+        {
+            PlayerCondition = Condition.Hungry;
+        }
+        else if (Smart <= 0)
+        {
+            PlayerCondition = Condition.Stupid;
+        }
+        else if (Happy <= 0)
+        {
+            PlayerCondition = Condition.Angry;
+        }
+        else if (Happy >= 80)
+        {
+            PlayerCondition = Condition.Happy;
+        }
+        else
+        {
+            PlayerCondition = Condition.Normal;
+        }
+    }
+    //プレイヤーのコンディションに応じた動きとアニメーション
+    public void PlayerConditionMoving()
+    {
+        if (PlayerCondition == Condition.Normal)
+        {
+            _playerAnimation.NormalSprite();
+            //ランダムウォーク(DoTween)
+            //他のプレイヤーに近づいて話しかける
+
+        }
+        else if (PlayerCondition == Condition.Tired)
+        {
+            _playerAnimation.TiredSprite();
+            //座り込むアニメーション
+        }
+        else if (PlayerCondition == Condition.Hungry)
+        {
+            _playerAnimation.HungrySprite();
+            //ランダムでうなだれるアニメーション
+        }
+        else if (PlayerCondition == Condition.Stupid)
+        {
+            _playerAnimation.StupidSprite();
+            //顔があほになるアニメーション
+        }
+        else if (PlayerCondition == Condition.Angry)
+        {
+            _playerAnimation.AngrySprite();
+            //怒りのアニメーション
+        }
+        else if(PlayerCondition == Condition.Happy)
+        {
+            _playerAnimation.HappySprite();
+            //Happyのアニメーション
+        }
+    }
+}
