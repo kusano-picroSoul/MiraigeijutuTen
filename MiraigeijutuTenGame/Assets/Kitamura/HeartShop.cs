3fad54a708141391a8268bd048486b1f95107c1f
Kitamura Haruki <90311063+629Kitamura@users.noreply.github.com>
2023-07-23 23:13:43 +0900

    [update]調整のための変数追加

--- MiraigeijutuTenGame/Assets/Kitamura/HeartShop.cs
+++ MiraigeijutuTenGame/Assets/Kitamura/HeartShop.cs
@@ -5,11 +5,31 @@ using UnityEngine.UI;
 
 public class HeartShop : MonoBehaviour
 {
+    /// <summary>TEXTにコンポーネントとして付いてる</summary>
     [SerializeField] AddHeart _addHeart;
+
     [SerializeField] Text _addHeartSeconds;
     [SerializeField] Text _touchAddHeart;
     [SerializeField] Text _heartCost0;
     [SerializeField] Text _heartCost1;
+    [Header("foods")]
+    [SerializeField] bool _rice;
+    [SerializeField] bool _chocolateBar;
+    [SerializeField] bool _protein;
+    [SerializeField] bool _pizzaAndCola;
+    [SerializeField] bool _ice;
+    [SerializeField] bool _saladChicken;
+    [SerializeField] bool _katsuCarry;
+    [SerializeField] bool _birthdayCake;
+    [SerializeField] bool _vitaminFood;
+
+    //変動する値
+    //小
+    float _small = 0.1f;
+    //中
+    float _mid = 0.2f;
+    //大
+    float _large = 0.3f;
 
     /// <summary> きずなパワーのレベル　※秒間きずなパワーレベル×１０のハートをゲット </summary>
     public int _friendPowerLevel = 0;
@@ -45,5 +65,10 @@ public class HeartShop : MonoBehaviour
                 _heartCost1.text = "cost " + (_level * _lovePowerLevel).ToString();
             }
         }
+    }
+
+    float Foods(float )
+    {
+
     }
 }
