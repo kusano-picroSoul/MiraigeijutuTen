1ebea8d88ba34cd17401e0ed89949248ca1a42c9
AkAtOmO <akatomo0923@au.com>
2023-07-25 00:38:08 +0900

    なでる動作の修正

--- MiraigeijutuTenGame/Assets/Akasaka/StrokingController.cs
+++ MiraigeijutuTenGame/Assets/Akasaka/StrokingController.cs
@@ -9,22 +9,37 @@ public class StrokingController : PlayerPresenter
     private PlayerAnimation _playerAnimation;
     Animator _jump;
     HeartShop _heartShop;
+    AddHeart _add;
     int heart;
+    float Heart;
+    PlayerStatus _status;
     private void Start()
     {
         _jump = GetComponent<Animator>();
         _playerAnimation = GetComponent<PlayerAnimation>();
         _heartShop = GetComponent<HeartShop>();
+        _add = GetComponent<AddHeart>();
+        _status = GetComponent<PlayerStatus>();
     }
     private void OnMouseDrag()
     {
         _playerAnimation.HappySprite();
-        heart = _heartShop._lovePowerLevel;
     }
     private new void OnMouseUp()
     {
         _playerAnimation.NormalSprite();
         _jump.Play("JumpMotion");
+        _status.Happy += 2;
+        heart = _heartShop._lovePowerLevel;
+        Heart = _add._heart;
+        if (heart < 500)
+        {
+            Heart += 1;
+        }
+        else
+        {
+            Heart += 2;
+        }
     }
 
 }
