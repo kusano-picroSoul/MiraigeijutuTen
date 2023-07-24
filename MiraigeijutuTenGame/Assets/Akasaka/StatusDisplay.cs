d9baea814fe59512c26d8b7b507a356b11c03454
AkAtOmO <akatomo0923@au.com>
2023-07-24 23:04:31 +0900

    ステータスウィンドウになかよし度追加

--- MiraigeijutuTenGame/Assets/Akasaka/StatusDisplay.cs
+++ MiraigeijutuTenGame/Assets/Akasaka/StatusDisplay.cs
@@ -16,6 +16,7 @@ public class StatusDisplay : MonoBehaviour
     Slider _hungryBar;
     Slider _happyBar;
     Slider _smartBar;
+    Slider _familiarityBar;
     //[SerializeField] Slider _helthBar;
     PlayerStatus _status;
     // Start is called before the first frame update
@@ -26,9 +27,11 @@ public class StatusDisplay : MonoBehaviour
         _hungryBar = GameObject.Find("Canvas/HungryBar").GetComponent<Slider>();
         _happyBar = GameObject.Find("Canvas/HappyBar").GetComponent<Slider>();
         _smartBar = GameObject.Find("Canvas/SmartBar").GetComponent<Slider>();
+        _familiarityBar = GameObject.Find("Canvas/FamiliarityBar").GetComponent<Slider>();
         _hungryBar.gameObject.SetActive(false);
-       _happyBar.gameObject.SetActive(false);
-       _smartBar.gameObject.SetActive(false);
+        _happyBar.gameObject.SetActive(false);
+        _smartBar.gameObject.SetActive(false);
+        _familiarityBar.gameObject.SetActive(false);
         _text.enabled = false;
         // _helthBar.gameObject.SetActive(false);
     }
@@ -43,12 +46,14 @@ public class StatusDisplay : MonoBehaviour
 
     public void OnMouseDown()
     {
-        _text.text = "Ç®Ç»Ç©Å@ :" + _status.Hungry + "\n"
-              + "Ç≤Ç´Ç∞ÇÒ :" + _status.Happy + "\n"
-              + "Ç©ÇµÇ±Ç≥ :" + _status.Smart;
+        _text.text = "Ç®Ç»Ç©       :" + _status.Hungry + "\n"
+                   + "Ç≤Ç´Ç∞ÇÒ    :" + _status.Happy + "\n"
+                   + "Ç©ÇµÇ±Ç≥    :" + _status.Smart +"\n"
+                   + "Ç»Ç©ÇÊÇµìx :" + _status.Familiarity;
         _hungryBar.value = _status.Hungry;
         _happyBar.value = _status.Happy;
         _smartBar.value = _status.Smart;
+        _familiarityBar.value = _status.Familiarity;
         Vector2 posi = transform.position;
         GameObject.Find("Canvas").transform.position = new Vector2(posi.x + 3.3f, posi.y);
         Debug.Log("êGÇ¡ÇΩ");
@@ -58,6 +63,7 @@ public class StatusDisplay : MonoBehaviour
             _hungryBar.gameObject.SetActive(true);
             _happyBar.gameObject.SetActive(true);
             _smartBar.gameObject.SetActive(true);
+            _familiarityBar.gameObject.SetActive(true);
             //_helthBar.gameObject.SetActive(true);
             _active = true;
         }
@@ -70,6 +76,7 @@ public class StatusDisplay : MonoBehaviour
             _hungryBar.gameObject.SetActive(false);
             _happyBar.gameObject.SetActive(false);
             _smartBar.gameObject.SetActive(false);
+            _familiarityBar.gameObject.SetActive(false);
             // _helthBar.gameObject.SetActive( false);
             _active = false;
         }
--- MiraigeijutuTenGame/Assets/Akasaka/StrokingController.cs
+++ MiraigeijutuTenGame/Assets/Akasaka/StrokingController.cs
@@ -7,14 +7,24 @@ using UnityEngine.UI;
 public class StrokingController : PlayerPresenter
 {
     private PlayerAnimation _playerAnimation;
-    private Animator _JunpMotion;
+    Animator _jump;
+    HeartShop _heartShop;
+    int heart;
     private void Start()
     {
+        _jump = GetComponent<Animator>();
         _playerAnimation = GetComponent<PlayerAnimation>();
-        
+        _heartShop = GetComponent<HeartShop>();
     }
     private void OnMouseDrag()
     {
         _playerAnimation.HappySprite();
+        heart = _heartShop._lovePowerLevel;
     }
+    private new void OnMouseUp()
+    {
+        _playerAnimation.NormalSprite();
+        _jump.Play("JumpMotion");
+    }
+
 }
